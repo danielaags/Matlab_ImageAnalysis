8c40d56dc801d33c3e887d78ada91c8233a4d413
Daniela Garcia <daniela.ags13@gmail.com>
2019-10-08 17:21:54 +0200

    Update ID_CFU.m
    Some segmentation included.

--- ID_CFU.m
+++ ID_CFU.m
@@ -1,7 +1,9 @@
+%Read file
 file = 'day5-40µl-nr_5';
 I = imread(strcat(file,'.tif'));
 
-%Get the image size to remove the rim of the petri dish
+%%%Remove uninterested region from the image%%%
+%get the image size to remove the rim of the petri dish
 imageSize = size(I);
 %center and radius of circle ([c_row, c_col, r]). The images are a bit
 %off. Center doesn't seem to be at 1024, 1024
@@ -13,23 +15,45 @@ croppedImage(:,:,1) = I(:,:,1).*mask;
 croppedImage(:,:,2) = I(:,:,2).*mask;
 croppedImage(:,:,3) = I(:,:,3).*mask;
 
-%CFUradius=100; % CFU, maximum size to take into account
-[centers, radii, metric] = imfindcircles(croppedImage,[20, 60], 'Sensitivity',0.92, 'EdgeThreshold', 0.04);
-%[centers, radii, metric] = imfindcircles(croppedImage,[20, 60],'Sensitivity',0.97);
-%[centers, radii, metric] = imfindcircles(croppedImage,[150, 200],'Sensitivity',0.97);
+imshow(croppedImage)
+
+%Find circles without segmentation
+%[centers, radii] = imfindcircles(croppedImage,[20, 60], 'Sensitivity',0.92, 'EdgeThreshold', 0.04);
+
+%Segmentation%%%
+grayImage = rgb2gray(croppedImage);
+%bg = gaussf(grayImage,1,'best');
+imshow(grayImage);
+%imshow(bg);
+%Trasformation to get into 0-255 RGB range
+imhist(uint8(gray_image/257))
+
+%For brighter colonies than the background
+grayImage_fd = grayImage > 30*257 & grayImage < 120*257; %imshow(grayImage_f)
+[centers_fd, radii_fd] = imfindcircles(grayImage_f,[20, 60], 'ObjectPolarity', 'dark');
+
+%For colonies similar to the background
+grayImage_fb = grayImage > 75; %imshow(grayImage_f)
+[centers_fb, radii_fb] = imfindcircles(grayImage_f,[20, 60], 'ObjectPolarity', 'bright');
+
+centers = [centers_fd ; centers_fb];
+data = table(centers);
+writetable(data,strcat(file,'_centers.txt'),'Delimiter','\t');
+
+radii = [radii_fd ; radii_fb];
+data = table(radii);
+writetable(data,strcat(file,'_radii.txt'),'Delimiter','\t');
 
 
-% [centers1, radii1, metric1] = imfindcircles(croppedImage,[20, 60],'Sensitivity',0.95);
-% [centers2, radii2, metric2] = imfindcircles(croppedImage,[250, 300],'Sensitivity',0.97);
-
-% data = table(centers);
-% writetable(data,strcat(file,'_centers.txt'),'Delimiter','\t');
+% %Plot the colonies found
+% c = figure;
+% subplot(1,2,1)
+% imshow(croppedImage);
 % 
-% data = table(radii);
-% writetable(data,strcat(file,'_radii.txt'),'Delimiter','\t');
+% subplot(1,2,2)
+% imshow(gray_image)
+% viscircles(centers,radii,'EdgeColor','b');
 
-
-%imshow(I);
+%Plot the colony number
 imshow(croppedImage);
 viscircles(centers,radii,'EdgeColor','b');
-
