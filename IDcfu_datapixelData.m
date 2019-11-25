16a46526732e6ab041fe8aa77776a37fabb888f6
Daniela Garcia <daniela.ags13@gmail.com>
2019-11-25 16:03:43 +0100

    Update IDcfu_datapixelData.m
    It only extracts pixel values at a specific time point in different plates. Pixel subtraction is done using the mask got in the segmentation process, this warranties to keep the centroids values the same.

--- IDcfu_datapixelData.m
+++ IDcfu_datapixelData.m
@@ -22,8 +22,9 @@ pixel_size=2.54/96; %cm
 
 %Read files. Batch of pictures from a plate taken along several days
 images = dir('*.tif');
-
+count = 0;
 for n = 1:length(images)
+%for n = 3:3
     file= images(n).name;
     filename = split(file, '.');
     %16-bit file, RGB range 0-2500
@@ -51,7 +52,11 @@ for n = 1:length(images)
     %Only take information from the red channel
     
     %Correct non-uniform ilumination. Adjust specific range
-    rgbI = imadjust(croppedImage(:,:,1), [0 0.60],[]);%imshow(rgbI);
+    rgbI = imadjust(croppedImage(:,:,1), [0 0.60],[]);
+    figure
+    imshow(rgbI)
+    print(strcat(filename{1},'-adjust'),'-dpng');
+    close;
     
     %top-hat filtering 
 %     se = strel('disk',100);
@@ -59,45 +64,51 @@ for n = 1:length(images)
 %     
 
     %Filter bright colonies
-    rgbBW = rgbI >=190;%imshow(rgbBW)
+    rgbBW = rgbI >=200;%imshow(rgbBW)
     %remove connected objects
     rgbI_nobord = imclearborder(rgbBW,8);%imshow(rgbI_nobord)
     %to fill up holes
-    rgbI_final = imfill(rgbI_nobord,'holes');%imshow(rgbI_final)       
-
+    rgbI_noholes = imfill(rgbI_nobord,'holes');%imshow(rgbI_final)       
+    %rgbI_final = rgbI_noholes;
+    %smooth object
+    seD = strel('diamond',5);
+    rgbI_final = imerode(rgbI_noholes,seD);
+    
+    
     %Find colonies using boundary. Save the data collected.
     %B, returns an array of pixel locations
     %L, label matrix of objects
     %n, number of objects (labels)
     %A, adjacent matrix
-    [B1,L1,n1,A1] = bwboundaries(rgbI_final,'noholes');      
-        figure
-        imshow(L)
-        hold on
-        for k = 1:length(B)
-           boundary = B{k};
-           plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 2)
-        end
+    [B1,L1,n1] = bwboundaries(rgbI_final,'noholes');      
+%         figure
+%         imshow(I)
+%         hold on
+%         for k = 1:length(B)
+%            boundary = B{k};
+%            plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 2)
+%         end
 
     rgbBW = rgbI < 50;%imshow(rgbBW)
     %remove connected objects
     rgbI_nobord = imclearborder(rgbBW,8);%imshow(rgbI_nobord)
+    %rgbI_final = rgbI_nobord;
     %smooth object
-    seD = strel('diamond',1);
+    seD = strel('diamond',5);
     rgbI_final = imerode(rgbI_nobord,seD);
 
     %Find colonies using boundary
-    [B2,L2,n2,A2] = bwboundaries(rgbI_final,'noholes');
+    [B2,L2,n2] = bwboundaries(rgbI_final,'noholes');
 
     if isempty(n2)
         B = B1;
         L = L1;
-        A = A1;
+        n_colonies = n1;
     else
         %BW image
         B = [B1; B2];
         L = (L1 | L2);
-        A = [A1; A2];
+        n_colonies = (n1+n2);
     end 
 
     figure
@@ -107,6 +118,7 @@ for n = 1:length(images)
 
     %get morphological stats
     stats=  regionprops(L, 'Centroid', 'Area', 'EquivDiameter', 'Perimeter', 'Circularity', 'Eccentricity');
+    Centroid = floor(cat(1,stats.Centroid));
     Eccentricity = cat(1,stats.Eccentricity);
     Area = cat(1,stats.Area);
     Diameter = cat(1,stats.EquivDiameter);
@@ -136,7 +148,7 @@ for n = 1:length(images)
     %%
     %Filter by area bigger than n and eccentricity, the closes to 1 the more
     %line segment, the closes to 0 the more circular shape
-    filter1 = find(Area > 200 & Area < 70000 & Eccentricity < 0.7); 
+    filter1 = find(Area > 200 & Area < 70000 & Eccentricity < 0.79); 
 
     %Find the elements close the the petri dish walls
     filter2 = zeros(length(filter1),1);
@@ -146,10 +158,10 @@ for n = 1:length(images)
     for i = 1:length(filter1)
         %imshow(croppedImage);
         %Fit a circle
-        xunit = floor(Diameter(filter1(i)) * cos(th) + Centroid(filter1(i),1));
-        yunit = floor(Diameter(filter1(i)) * sin(th) + Centroid(filter1(i),2));
+        xunit = floor(Diameter(filter1(i))/2 * cos(th) + Centroid(filter1(i),1));
+        yunit = floor(Diameter(filter1(i))/2 * sin(th) + Centroid(filter1(i),2));
         %Find within the boundaries. Check ci variable
-        mean_xy = mean(yunit>225 & yunit<1875 & xunit>225 & xunit<1875);
+        mean_xy = mean(yunit>200 & yunit<1875 & xunit>200 & xunit<1875);
         if mean_xy == 1
            filter2(i) = filter1(i);
         end
@@ -157,24 +169,37 @@ for n = 1:length(images)
 
     filter2 = nonzeros(filter2);
     
-    diameter = floor(Diameter(filter2));
+    %%
+    %Get std of the selected colonies
+    std_red = zeros(1, length(filter2));
+    std_green = zeros(1, length(filter2));
+    std_blue = zeros(1, length(filter2));
+    
+    for m = 1:length(filter2)
+        std_red(m) = std(double(statsPixel_red(filter2(m)).PixelValues));
+        std_green(m) = std(double(statsPixel_green(filter2(m)).PixelValues));
+        std_blue(m) = std(double(statsPixel_red(filter2(m)).PixelValues));    
+    end
 
     %%
+    diameter = floor(Diameter(filter2));
+
     %%Plot the colonies found and save data
     %Labels
-    colony = cell(1,length(diameter)); 
-    label = cell(length(diameter),1);
-    ID = cell(length(diameter),1);
+    colony = strings(length(diameter),1); 
+    label = strings(length(diameter),1);
+    ID = strings(length(diameter),1);
     %Give labels
     for i = 1:length(diameter)
-        colony{i} = int2str(i);
-        label{i}= strcat(day,'-',int2str(n),'-',int2str(i));
-        ID{i}=strcat(int2str(n),'-',int2str(i));
+        colony(i) = int2str(i);
+        label(i)= strcat(day,'-',int2str(n),'-',int2str(i));
+        ID(i)=strcat(int2str(n),'-',int2str(i));
     end
+    
     % 
-    centroid= floor(cat(1,stats(filter2).Centroid));
+    centroid= Centroid(filter2,:);
     figure
-    imshow(croppedImage);
+    imshow(I);
     viscircles(centroid,diameter/2,'EdgeColor','b');
     text(centroid(:,1), centroid(:,2), colony);
     print(strcat(filename{1},'IDs'),'-dpng');
@@ -183,10 +208,16 @@ for n = 1:length(images)
 %Check how to save the data, all the variables bwboundaries, regiongprops
 %morphological and pixel values
 %Save data
-    statsData = struct('label', label, 'ID', ID, 'centroid', centroid, 'perimeter', perimeter, 'area', area, 'radii', radii, 'eccentricity', eccentricity, 'circularity', circularity);
+    statsData = struct('label', label, 'ID', ID, 'centroid', centroid,...
+        'area', cat(1,stats(filter2).Perimeter), 'diameter', cat(1,stats(filter2).EquivDiameter),...
+        'perimeter', cat(1,stats(filter2).Perimeter), 'circularity', cat(1,stats(filter2).Circularity),...
+        'eccentricity', cat(1,stats(filter2).Eccentricity), 'R_mean', cat(1,statsPixel_red(filter2).MeanIntensity),...
+        'R_std', std_red, 'G_mean', cat(1,statsPixel_green(filter2).MeanIntensity), 'G_std', std_green, 'B_mean', cat(1,statsPixel_blue(filter2).MeanIntensity),...
+        'B_std', std_red);
     save(strcat(filename{1},'-data.mat'), 'statsData');
-
-
+    
+    %count number of colonies
+    count = count + length(filter2);
 end 
 %end
 
