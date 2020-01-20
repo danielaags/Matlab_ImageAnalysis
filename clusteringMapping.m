ba818b25b9cefd6688a5fa93a487bff38001d357
Daniela Garcia <daniela.ags13@gmail.com>
2020-01-20 14:07:46 +0100

    Update clusteringMapping.m
    Generation of thumbnails added.

--- clusteringMapping.m
+++ clusteringMapping.m
@@ -1,9 +1,12 @@
-clear all
+% clear all
+% close all
 images = dir('*.tif');
 load('pixel_bio-Data.mat', 'data_isolation')
 
 %Extract data to use
 %X = cat(1,data_isolation.RGB_mean); %meanArea
+%X = cat(1,data_isolation.RGB_std); %stdArea
+%X = cat(1,data_isolation.RGBt_mean); %areaTrasversal
 X = cat(1,data_isolation.RGBt_std); %stdTrasversal
 
 %R saves the row numbers that were removed
@@ -45,6 +48,8 @@ for j = 1:length(images)
     %j=1;
     file= images(j).name;
     I = imread(file);
+    output = split(file, '.');
+
     
     %Find index where the plate belongs
     temp_idx = find(plate == j);
@@ -59,37 +64,43 @@ for j = 1:length(images)
     
     %Gets the cluster that correspond to the specific plate
     %clusters = unique(temp_cluster);
-    figure
-    imshow(I);
-    hold on
+%     figure
+%     imshow(I);
+%     hold on
     %Goes along the different clusters in the plate and plots
-%     for i = 1:length(clusters) 
-%         idx_c = find(temp_cluster == clusters(i));
-%         viscircles(temp_centroid(idx_c,:), temp_radii(idx_c), 'Color', col(clusters(i),:));
-%     end  
+
     for i = 1:length(temp_cluster_mean) 
         y = temp_centroid(i,2);
         x = temp_centroid(i,1);
-        r = temp_radii(i);
+        r = temp_radii(i) + 10;
         %rectangle('Position', [x-r y-r r r], 'EdgeColor', col(temp_cluster_sd(i),:), 'LineWidth', 2);
-        rectangle('Position', [x-r y-r r*2 r*2], 'EdgeColor', col(temp_cluster_mean(i),:), 'LineWidth', 2);
-        
+        %rectangle('Position', [x-r y-r r*2 r*2], 'EdgeColor', col(temp_cluster_mean(i),:), 'LineWidth', 2);
+        B=I(y-r:y+r, x-r:x+r,:);
+        figure
+        imshow(B);
+        print(strcat('c',string(temp_cluster_mean(i)),'_',output{1}),'-dpng');
+        close;       
     end 
-    hold off
+    %hold off
     
-    output = split(file, '.');
-    print(output{1},'-dpng');
-    close;
+%     output = split(file, '.');
+%     print(output{1},'-dpng');
+%     close;
+
 end
 
 %Remove missing values and add clustering information
 T = struct2table(data_isolation);
 dataT = T(nrow, :);
 %dataT.sumActivity = sum(table2array(dataT(:,26:38)), 2);
-%dataT.cluster = C0;
-%writetable(dataT, 'pixel_bio_cluster-Data.xls')
+dataT.cAreaMean = C0_meanRGB;
+dataT.cAreaStd = C0_stdRGB;
+dataT.cTransMean = C0_RGBtmean;
+dataT.cTransStd = C0;
 
 %Save as excel
+writetable(dataT, 'pixel_bio_Diff-cluster-Data.xls')
+
 
 %Save as .mat
 cluster_data_isolation = table2struct(dataT);
